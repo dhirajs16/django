a8164c40c91ba2562c592e194f50e76c80a2b585
dhirajs16 <sarrafd16@gmail.com>
2024-10-24 16:11:28 +0545

    CRUD operations

--- drf_tutorial/myapp/urls.py
+++ drf_tutorial/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.StudentAPIView.as_view(), name='StudentAPIView'),
+    path('api/<int:id>/', views.StudentInfo.as_view(), name='StudentInfo'),
 ]
