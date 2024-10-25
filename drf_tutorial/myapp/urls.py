3c6cc653ca32c8e7174fbead057e272d6f2100b7
dhirajs16 <sarrafd16@gmail.com>
2024-10-25 06:28:08 +0545

    Revised

--- drf_tutorial/myapp/urls.py
+++ drf_tutorial/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.StudentAPIView.as_view(), name='StudentAPIView'),
+    # path('', views.StudentAPIView.as_view(), name='StudentAPIView'),
     path('api/<int:id>/', views.StudentInfo.as_view(), name='StudentInfo'),
 ]
--- drf_tutorial/myapp/views.py
+++ drf_tutorial/myapp/views.py
@@ -1,77 +1,152 @@
+# from django.shortcuts import render
+# from rest_framework.views import APIView
+# from rest_framework import status
+# from rest_framework.response import Response
+# from .models import Student
+# from .serializers import StudentSerializer
+
+# # GET all and POST
+# class StudentAPIView(APIView):
+#     def get(self, request):
+#         obj = Student.objects.all()
+#         serializer = StudentSerializer(obj, many = True)
+#         return Response(data = serializer.data, status = status.HTTP_200_OK)
+    
+#     def post(self, request):
+#         serializer = StudentSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# # GET one, PUT, PATCH, DELETE
+# class StudentInfo(APIView):
+#     def get(self, request, id):
+#         try:
+#             obj = Student.objects.get(id = id)
+
+#         except Student.DoesNotExist:
+#             msg = {'message': 'Not Found'}
+#             return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        
+#         serializer = StudentSerializer(obj)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+#     def put(self, request, id):
+#         try:
+#             obj = Student.objects.get(id = id)
+
+#         except Student.DoesNotExist:
+#             msg = {'message': 'Not Found'}
+#             return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        
+#         serializer = StudentSerializer(obj, data = request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+#     def patch(self, request, id):
+#         try:
+#             obj = Student.objects.get(id = id)
+
+#         except Student.DoesNotExist:
+#             msg = {'message': 'Not Found'}
+#             return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        
+#         serializer = StudentSerializer(obj, data = request.data, partial = True)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+#     def delete(self, request, id):
+#         try:
+#             obj = Student.objects.get(id = id)
+
+#         except Student.DoesNotExist:
+#             return Response({'message':'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+        
+#         obj.delete()
+#         return Response({'message': 'Deleted Successfully'}, status=status.HTTP_204_NO_CONTENT)
+        
+
+
+
+
+
+
+
+
+
+
+
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Student
 from .serializers import StudentSerializer
 
-# GET all and POST
-class StudentAPIView(APIView):
-    def get(self, request):
-        obj = Student.objects.all()
-        serializer = StudentSerializer(obj, many = True)
-        return Response(data = serializer.data, status = status.HTTP_200_OK)
-    
-    def post(self, request):
-        serializer = StudentSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# GET one, PUT, PATCH, DELETE
+
 class StudentInfo(APIView):
     def get(self, request, id):
         try:
             obj = Student.objects.get(id = id)
-
         except Student.DoesNotExist:
-            msg = {'message': 'Not Found'}
-            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message':'Not Found'}, status=status.HTTP_404_NOT_FOUND)
         
         serializer = StudentSerializer(obj)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+
     def put(self, request, id):
         try:
             obj = Student.objects.get(id = id)
-
         except Student.DoesNotExist:
-            msg = {'message': 'Not Found'}
-            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message':'Not Found'}, status=status.HTTP_404_NOT_FOUND)
         
         serializer = StudentSerializer(obj, data = request.data)
+        if not serializer.is_valid():
+            return Response({'message':'Invalid upadate'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
     def patch(self, request, id):
         try:
             obj = Student.objects.get(id = id)
-
         except Student.DoesNotExist:
-            msg = {'message': 'Not Found'}
-            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message':'Not Found'}, status=status.HTTP_404_NOT_FOUND)
         
         serializer = StudentSerializer(obj, data = request.data, partial = True)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        if not serializer.is_valid():
+            return Response({'message':'Invalid upadate'}, status=status.HTTP_400_BAD_REQUEST)
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
     
 
     def delete(self, request, id):
         try:
             obj = Student.objects.get(id = id)
-
         except Student.DoesNotExist:
-            return Response({'message':'Not Found'}, status=status.HTTP_404_NOT_FOUND)
-        
+            return Response({'message': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+
         obj.delete()
-        return Response({'message': 'Deleted Successfully'}, status=status.HTTP_204_NO_CONTENT)
-        
+        return Response({'message':'Deleted Successfully'}, status=status.HTTP_205_RESET_CONTENT)
+
+
+
+
+
+
+
